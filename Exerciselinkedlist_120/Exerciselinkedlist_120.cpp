26e0e81ec751e36f42eb7baa5d005ee46ceca0f9
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2023-06-08 15:05:44 +1200

    Add project files.

---
+++ Exerciselinkedlist_120/Exerciselinkedlist_120.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node {
+public:
+	int rollNumber;
+	string name;
+	Node* next;
+};
+
+class CircularLinkedList {
+private:
+	Node* LAST;
+
+public:
+	CircularLinkedList() {
+		LAST = NULL;
+	}
+	void addNode();
+	bool search(int rollno, Node** previous, Node** current);
+	bool listEmpty();
+	bool delNode();
+	void trasverse();
+};
+
+void CircularLinkedList::addNode() {		//allocate memory for the new node
+	int rollNumber;
+	string name;
+	cout << "\nEnter the roll number:  ";
+	cin >> rollNumber;
+	cout << "\nEnter of the name: ";
+	cin >> name;
+	Node* newNode = new Node();
+	newNode->rollNumber = rollNumber;
+	newNode->name = name;
+}
+
+bool CircularLinkedList::search(int rollno, Node** previous, Node** current) {
+	*previous = LAST->next;
+	*current = LAST->next;
+
+	while (*current != LAST) {
+		if (rollno == (*current)->rollNumber) {
+			return true;
+		}
+		*previous = *current;
+		*current = (*current)->next;
+	}
+	if (rollno == LAST->rollNumber) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool CircularLinkedList::listEmpty() {			
+	return LAST == NULL;
+}
+
+bool CircularLinkedList::delNode() {			//
+	Node* previous, * current;
+	previous = current = NULL;
+	if (current->next != NULL)
+		current->next = previous;
+}
+
+void CircularLinkedList::trasverse() {
+	if (listEmpty()) {
+		cout << "\nList is empty\n";
+	}
+	else {
+		cout << "\nRecord in the list are\n";
+		Node* currentNode = LAST->next;
+		while (currentNode != LAST) {
+			cout << currentNode->rollNumber << "  " << currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+		cout << LAST->rollNumber << "  " << LAST->name << endl;
+	}
+}
+
+int main() {
+	CircularLinkedList obj;
+	while (true) {
+		try {
+			cout << "\nMenu" << endl;
+			cout << "1. Add a record to the list" << endl;
+			cout << "2. Delete a record from the list" << endl;
+			cout << "3. View all the records in the list" << endl;
+			cout << "4. Exit" << endl;
+			cout << "\nEnter your choice (1-5): ";
+
+			char ch;
+			cin >> ch;
+
+			switch (ch) {
+			case '1': {
+				obj.addNode();
+				break;
+			}
+			case '2': {
+				obj.search();
+				break;
+			}
+			case '3': {
+				obj.trasverse();
+				break;
+			}
+			case '4': {
+				return 0;
+			}
+			default: {
+				cout << "Invalid option" << endl;
+				break;
+			}
+			}
+		}
+		catch (exception& e) {
+			cout << e.what() << endl;
+		}
+	}
+	return 0;
+}
